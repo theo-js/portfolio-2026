e437c0a325b6bb20436bdc8bbd33ebeac5049dd1
Theo Bayenet <dev.js.theo@gmail.com>
2026-02-28 20:07:55 +0100

    feat: add 'show more' themes button

--- components/layout/TopBar/actions/common/ThemeSelector/fieldsets/ColorTheme.tsx
+++ components/layout/TopBar/actions/common/ThemeSelector/fieldsets/ColorTheme.tsx
@@ -10,10 +10,11 @@ import {
   FieldTitle,
 } from '@/components/ui/field';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
-import type { FC } from 'react';
+import { useState, type FC } from 'react';
 import { useCustomVariantsContext } from '@/core/theming/CustomVariants/CustomVariantsContextProvider';
 import type { ColorTheme } from '@/core/theming/ThemeProvider';
 import { ThemeSvg } from '../theme-svg/default';
+import { Button } from '@/components/ui/button';
 
 const colorThemesDictionary: Record<ColorTheme, { titleTKey: string }> = {
   plasma: { titleTKey: 'options.plasma.title' },
@@ -23,14 +24,11 @@ const colorThemesDictionary: Record<ColorTheme, { titleTKey: string }> = {
   copper: { titleTKey: 'options.copper.title' },
   emerald: { titleTKey: 'options.emerald.title' },
 };
-const colorThemes = Object.keys(colorThemesDictionary).map((key) => ({
-  id: key as ColorTheme,
-  ...colorThemesDictionary[key as ColorTheme],
-}));
 
 export const ColorThemeFieldset: FC = () => {
   const t = useTranslations('topbar.theme-selector.menu.fieldsets.color-theme');
   const { colorTheme, setColorTheme } = useCustomVariantsContext();
+  const { colorThemes, appendNextColorThemes, canAppendMoreColorThemes } = useDisplayThemes();
 
   return (
     <AccordionItem value="color-theme">
@@ -42,7 +40,7 @@ export const ColorThemeFieldset: FC = () => {
           </FieldLegend>
         </AccordionTrigger>
 
-        <AccordionContent>
+        <AccordionContent className="flex flex-col items-end gap-4">
           <RadioGroup
             value={colorTheme}
             onValueChange={(value) => setColorTheme(value as ColorTheme)}
@@ -69,8 +67,32 @@ export const ColorThemeFieldset: FC = () => {
               );
             })}
           </RadioGroup>
+
+          {canAppendMoreColorThemes && (
+            <Button variant="ghost" onClick={appendNextColorThemes}>
+              {t('show-more')}
+            </Button>
+          )}
         </AccordionContent>
       </FieldSet>
     </AccordionItem>
   );
 };
+
+const THEMES_PAGE_SIZE = 3;
+const allColorThemes = Object.keys(colorThemesDictionary).map((key) => ({
+  id: key as ColorTheme,
+  ...colorThemesDictionary[key as ColorTheme],
+}));
+
+function useDisplayThemes() {
+  const [numberOfThemesToShow, setNumberOfThemesToShow] = useState(THEMES_PAGE_SIZE);
+  const themesToShow = allColorThemes.slice(0, numberOfThemesToShow);
+
+  return {
+    colorThemes: themesToShow,
+    appendNextColorThemes: () =>
+      setNumberOfThemesToShow((prevState) => prevState + THEMES_PAGE_SIZE),
+    canAppendMoreColorThemes: themesToShow.length < allColorThemes.length,
+  };
+}
