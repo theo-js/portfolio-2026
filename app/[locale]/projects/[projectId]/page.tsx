76c146c4be01d4cdf235607cccfa6dbf81bd0015
Theo Bayenet <dev.js.theo@gmail.com>
2026-03-06 23:13:28 +0100

    feat: setup next-mdx for project details page

---
+++ app/[locale]/projects/[projectId]/page.tsx
@@ -0,0 +1,32 @@
+import type { Locale } from 'next-intl';
+import fs from 'fs';
+import path from 'path';
+import { notFound } from 'next/navigation';
+
+export const generateStaticParams = async () => {
+  const projectIds = fs.readdirSync(
+    path.join(process.cwd(), 'app/[locale]/projects/[projectId]/_projectIds'),
+  );
+  const locales = (process.env.NEXT_PUBLIC_AVAILABLE_LOCALES ?? '').split(',');
+  return locales.flatMap((locale) => projectIds.map((projectId) => ({ projectId, locale })));
+};
+
+const ProjectDetailsPage = async ({
+  params: paramsPromise,
+}: {
+  params: Promise<{ locale: Locale; projectId: string }>;
+}) => {
+  const params = await paramsPromise;
+
+  try {
+    const MdxFile = (await import(`./_projectIds/${params.projectId}/${params.locale}.mdx`))
+      .default;
+    return <MdxFile />;
+  } catch (e) {
+    return notFound();
+  }
+};
+
+export const dynamicParams = false;
+
+export default ProjectDetailsPage;
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from 'next';
+import createMDXPlugin from '@next/mdx';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 };
 
+const withMDX = createMDXPlugin({});
+
 const withNextIntl = createNextIntlPlugin('./core/i18n/request.ts');
-export default withNextIntl(nextConfig);
+export default withMDX(withNextIntl(nextConfig));
