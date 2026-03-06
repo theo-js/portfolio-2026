5314018f32e785c9a54c0e1e3ca22f03af9c733a
Theo Bayenet <dev.js.theo@gmail.com>
2026-03-06 01:38:50 +0100

    feat: move project cards to carousel

--- components/section/sections/Projects/index.tsx
+++ components/section/sections/Projects/index.tsx
@@ -1,24 +1,51 @@
-import { type FC } from 'react';
+import type { FC } from 'react';
 import { ExternalLinkIcon, GithubIcon } from 'lucide-react';
 import { BaseSection } from '../../components/BaseSection';
 import { SectionId } from '../../SectionId.enum';
 import { Reveal } from '@/components/ui/reveal';
-import { getTranslations } from 'next-intl/server';
+import { Button } from '@/components/ui/button';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselIndicator,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 import { ProjectCard } from './ProjectCard';
 import { projects } from './constants';
-import { Button } from '@/components/ui/button';
+import { getTranslations } from 'next-intl/server';
 
 export const ProjectsSection: FC = async () => {
   const t = await getTranslations('sections.projects');
 
   return (
     <BaseSection id={SectionId.Projects}>
-      {/* Projects grid */}
-      <Reveal className="grid! gap-8 md:grid-cols-2">
-        {projects.map((project, index) => (
-          <ProjectCard key={project.titleTKey} {...{ project, index }} />
-        ))}
-      </Reveal>
+      {/* Projects carousel */}
+      <Carousel
+        opts={{
+          align: 'start',
+          loop: true,
+          skipSnaps: true,
+        }}
+      >
+        <div className="contents gap-2 md:flex">
+          <CarouselPrevious className="hidden xl:grid" />
+          <CarouselContent>
+            {projects.map((project, index) => (
+              <CarouselItem
+                key={project.titleTKey}
+                className="grid basis-[calc(100%-2rem)] md:basis-[calc(50%-2rem)]"
+              >
+                <ProjectCard {...{ project, index }} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselNext className="hidden xl:grid" />
+        </div>
+
+        <CarouselIndicator slidesLength={projects.length} className="mt-4" />
+      </Carousel>
 
       {/* CTA */}
       <Reveal animation="scaleIn" className="mx-auto mt-16 w-fit text-center">
