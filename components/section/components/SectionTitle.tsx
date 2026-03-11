8e47d0615279b3e37edc6ea1af203977aabe8837
Theo Bayenet <dev.js.theo@gmail.com>
2026-03-11 23:58:48 +0100

    fix: fix position of question mark tooltip trigger

--- components/section/components/SectionTitle.tsx
+++ components/section/components/SectionTitle.tsx
@@ -10,22 +10,24 @@ export const SectionTitle: React.FC<React.PropsWithChildren<SectionTitleProps>> 
   questionMark,
   children,
 }) => (
-  <h2 className="from-primary via-secondary to-tertiary glass:text-white relative mb-12 bg-gradient-to-r bg-clip-text py-4 text-center text-5xl text-transparent md:text-6xl md:tracking-tight">
+  <h2 className="from-primary via-secondary to-tertiary glass:text-white relative mx-auto mb-12 w-fit bg-gradient-to-r bg-clip-text py-4 text-center text-5xl text-transparent md:text-6xl md:tracking-tight">
     {children}
     {questionMark && (
-      <Tooltip>
-        <TooltipTrigger className="absolute bottom-[.66em]" asChild>
-          <Button
-            variant="ghost"
-            size="icon-sm"
-            className="text-primary rounded-full text-2xl font-bold"
-          >
-            ?
-          </Button>
-        </TooltipTrigger>
+      <div className="absolute -right-10 bottom-[.66em]">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon-sm"
+              className="text-primary hidden rounded-full text-3xl font-bold md:flex"
+            >
+              ?
+            </Button>
+          </TooltipTrigger>
 
-        <TooltipContent className="max-w-75">{questionMark}</TooltipContent>
-      </Tooltip>
+          <TooltipContent className="hidden max-w-75 md:block">{questionMark}</TooltipContent>
+        </Tooltip>
+      </div>
     )}
   </h2>
 );
