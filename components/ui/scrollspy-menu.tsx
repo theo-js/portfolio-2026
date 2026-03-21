8514cf75d05ffd40d79e4b7f2584765ef91a0b22
Theo Bayenet <dev.js.theo@gmail.com>
2026-03-21 16:34:37 +0100

    feat: add scrollspy menu

---
+++ components/ui/scrollspy-menu.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { useSectionObserver } from 'react-use-section-observer';
+
+interface ScrollspyMenuProps {
+  links: {
+    id: string;
+    label: string;
+  }[];
+  className?: string;
+}
+
+export const ScrollspyMenu = ({ links, className }: ScrollspyMenuProps) => {
+  useSectionObserver({
+    ids: links.map((link) => link.id),
+    activeClassName: 'text-primary! glass:text-white!',
+  });
+  return (
+    <nav className={cn('shrink-0', className)}>
+      <ul className="sticky top-22 flex flex-col gap-6">
+        {links.map((link) => (
+          <li key={link.id} className="ml-auto text-right leading-snug">
+            <a
+              href={`#${link.id}`}
+              className="text-foreground/80 hover:text-foreground focus-visible:text-foreground font-semibold"
+            >
+              {link.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
