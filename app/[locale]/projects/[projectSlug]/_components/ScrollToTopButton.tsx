126466a40cd1f6b330766c80655aec5750ca20a8
Theo Bayenet <dev.js.theo@gmail.com>
2026-03-21 17:46:21 +0100

    style(project details page): add backdrop blur to floating button

--- app/[locale]/projects/[projectSlug]/_components/ScrollToTopButton.tsx
+++ app/[locale]/projects/[projectSlug]/_components/ScrollToTopButton.tsx
@@ -12,7 +12,7 @@ export const ScrollToTopButton: FC = () => {
   return (
     <Button
       onClick={() => scrollTo({ left: 0, top: 0, behavior: 'smooth' })}
-      className="group fixed right-6 bottom-5 size-16 rounded-full opacity-100 transition-discrete duration-300 starting:opacity-0"
+      className="group fixed right-6 bottom-5 size-16 rounded-full opacity-100 backdrop-blur-xl transition-discrete duration-300 starting:opacity-0"
       variant="outline"
     >
       <ArrowUpIcon className="size-6 group-hover:animate-bounce group-focus:animate-bounce" />
