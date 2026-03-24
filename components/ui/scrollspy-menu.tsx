31a5494012db51ed29c7848e6bfeef65dfbb2a76
Theo Bayenet <dev.js.theo@gmail.com>
2026-03-24 21:55:39 +0100

    fix: add scope to the scrollspy link selector to ensure it only targets links within this menu

--- components/ui/scrollspy-menu.tsx
+++ components/ui/scrollspy-menu.tsx
@@ -3,6 +3,8 @@
 import { cn } from '@/lib/utils';
 import { useSectionObserver } from 'react-use-section-observer';
 
+const SCROLLSPY_MENU_CLASSNAME = 'scrollspy-menu';
+
 interface ScrollspyMenuProps {
   links: {
     id: string;
@@ -14,10 +16,16 @@ interface ScrollspyMenuProps {
 export const ScrollspyMenu = ({ links, className }: ScrollspyMenuProps) => {
   useSectionObserver({
     ids: links.map((link) => link.id),
-    activeClassName: 'text-primary! glass:text-white!',
+    activeClassName: 'text-primary! glass:text-white! glass:underline',
+    linkAccessors: {
+      getIsLinkActive: ({ link, activeElement }) =>
+        link.getAttribute('href') === `#${activeElement.id}`,
+      // Add scope to the selector to ensure it only targets links within this menu
+      getLinkSelector: (linkId) => `.${SCROLLSPY_MENU_CLASSNAME} a[href="#${linkId}"]`,
+    },
   });
   return (
-    <nav className={cn('shrink-0', className)}>
+    <nav className={cn(SCROLLSPY_MENU_CLASSNAME, 'shrink-0', className)}>
       <ul className="sticky top-33 flex flex-col gap-6">
         {links.map((link) => (
           <li key={link.id} className="leading-snug">
