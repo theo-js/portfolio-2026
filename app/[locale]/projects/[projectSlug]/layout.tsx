b085e028dc87d14bd0d168394616326c539e6721
PC-T-BAYENET\devjs <dev.js.theo@gmail.com>
2026-07-14 16:57:01 +0200

    feat: add metadata to project details page layout

--- app/[locale]/projects/[projectSlug]/layout.tsx
+++ app/[locale]/projects/[projectSlug]/layout.tsx
@@ -1,6 +1,45 @@
 import type { PropsWithChildren } from 'react';
 import { SectionId } from '@/components/section/SectionId.enum';
 import { LongPostLayout } from '@/components/layout/LongPostLayout';
+import type { Metadata } from 'next';
+import { projects } from '@/data-access/static/project';
+import { getTranslations } from 'next-intl/server';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ projectSlug: string }>;
+}): Promise<Metadata> {
+  const t = await getTranslations('sections.projects');
+  const { projectSlug } = await params;
+  const project = projects.find(({ slug }) => slug === projectSlug);
+  const title = project ? `${t(project.titleTKey)} | Théo Bayenet` : undefined;
+  const description = project ? t(project.descriptionTKey) : undefined;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      images: project
+        ? {
+            url: project.imageUrl,
+            alt: title,
+          }
+        : undefined,
+    },
+    twitter: {
+      card: 'summary',
+      title,
+      description,
+      images: project
+        ? {
+            url: project.imageUrl,
+            alt: title,
+          }
+        : undefined,
+    },
+  };
+}
 
 export default function ProjectLayout({ children }: PropsWithChildren) {
   return (
